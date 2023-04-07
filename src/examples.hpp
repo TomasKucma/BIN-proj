0c1519f8269b9da818da1f6a86e6bdec9b27a8a4
xkucma00 <xkucma00@vutbr.cz>
2023-04-07 18:18:04 +0200

    Add examples

---
+++ src/examples.hpp
@@ -0,0 +1,9 @@
+#include "cgp.hpp"
+
+const std::vector<std::vector<Bitmap>> ADDER_2b_EXPECTED_OUTS{
+    {0b0101101001011010U}, // out 0
+    {0b0011011011001001U}, // out 1
+    {0b0000000100110111U}, // out 2
+};
+const size_t ADDER_2b_ITERATION_COUNT = 100000;
+const CGP ADDER_2B(4, ADDER_2b_EXPECTED_OUTS, 8, 8, 5, 10, 10);
