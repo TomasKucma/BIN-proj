1a67f4a87eca1c4f1bb1b577fa31fc94f811f4d5
xkucma00 <xkucma00@vutbr.cz>
2023-04-10 16:34:00 +0200

    Update main with new example

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,22 @@
 #include "examples.hpp"
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-    CGP cgp(ADDER_2b);
+void test_cgp(CGP cgp, const size_t iteration_count) {
     auto best = cgp.run_evolution(ADDER_2b_ITERATION_COUNT);
     auto best_chromosome = std::get<const Chromosome &>(best);
     auto best_fitness = std::get<size_t>(best);
-    cgp.print_parameters();
+    cgp.print_parameters();                // DEBUG
     std::cout << "Best chromosome:\n";     // DEBUG
     cgp.print_chromosome(best_chromosome); // DEBUG
     std::cout << "\nBest fitness ";        // DEBUG
     cgp.print_fitness(best_fitness);       // DEBUG
     std::cout << "\n\n";                   // DEBUG
+}
+
+int main(int argc, char *argv[]) {
+    std::cout << "CGP for 2bit adder:\n\n"; // DEBUG
+    test_cgp(ADDER_2b, ADDER_2b_ITERATION_COUNT);
+    std::cout << "CGP for 7 input median:\n\n"; // DEBUG
+    test_cgp(MEDIAN_7, MEDIAN_7_ITERATION_COUNT);
     return 0;
 }
