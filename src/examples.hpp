5e4adfc2f0c65a2cae2f3388a8b6a4f82e10ee4d
xkucma00 <xkucma00@vutbr.cz>
2023-04-10 16:32:13 +0200

    Add median example

--- src/examples.hpp
+++ src/examples.hpp
@@ -10,6 +10,8 @@
 
 #include "cgp.hpp"
 
+// Adder with 2b inputs
+
 const std::vector<std::vector<Bitmap>> ADDER_2b_EXPECTED_OUTS{
     {0b0101101001011010U}, // out 0
     {0b0011011011001001U}, // out 1
@@ -17,5 +19,15 @@ const std::vector<std::vector<Bitmap>> ADDER_2b_EXPECTED_OUTS{
 };
 const size_t ADDER_2b_ITERATION_COUNT = 100000;
 const CGP ADDER_2b(4, ADDER_2b_EXPECTED_OUTS, 8, 8, 5, 10, 10);
+
+// Median with 7 inputs
+
+const std::vector<std::vector<Bitmap>> MEDIAN_7_EXPECTED_OUTS{
+    // out 0
+    {0b0000000000000001000000010001011100000001000101110001011101111111,
+     0b0000000100010111000101110111111100010111011111110111111111111111},
+};
+const size_t MEDIAN_7_ITERATION_COUNT = 1000000;
+const CGP MEDIAN_7(7, MEDIAN_7_EXPECTED_OUTS, 6, 4, 4, 10, 5);
 
 #endif // EXAMPLES_HPP
